753879cadaa9f0969f95b76adf603892622c6716
Jeroen Ruijgers <jeroen@salandur.nl>
2020-12-16 23:12:15 -0700

    Day 13: find bus schedule

--- app/day13.rb
+++ app/day13.rb
@@ -5,12 +5,30 @@ time = file.gets.to_i
 bus_times = file.gets.split(",").map { |v| v == "x" ? "x" : v.to_i }
 busses = bus_times.reject { |b| b == "x" }
 
+def next_departure(bus, time)
+  (1.0 * time / bus).ceil * bus
+end
+
 # Part 1
-next_departures = busses.map { |x| [x, (1.0 * time / x).ceil * x] }.to_h
+next_departures = busses.map { |b| [b, next_departure(b, time)] }.to_h
 next_bus_to_depart = next_departures.sort_by { |_k, v| v }.first
 next_bus = next_bus_to_depart[0]
 departs_at = next_bus_to_depart[1]
 
 wait_time = departs_at - time
 
-puts "Day  13.1: have to wait for #{wait_time} for bus #{next_bus} (#{(wait_time * next_bus).to_s.green})"
+puts "Day 13.1: have to wait for #{wait_time} for bus #{next_bus} (#{(wait_time * next_bus).to_s.green})"
+
+timestamp = 0
+step = 1
+
+busses.each do |bus|
+  bus_adjustment = bus_times.index(bus)
+
+  while (timestamp + bus_adjustment) % bus != 0
+    timestamp += step
+  end
+  step *= bus
+end
+
+puts "Day 13.2: time is #{timestamp.to_s.green}"
