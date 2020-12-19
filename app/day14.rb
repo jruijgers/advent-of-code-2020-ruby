77d38bef363c2abc4d51d6b4d31f46b927a34b88
Jeroen Ruijgers <jeroen@salandur.nl>
2020-12-19 11:20:37 -0700

    Day 14: find sum of masked addresses

--- app/day14.rb
+++ app/day14.rb
@@ -26,8 +26,37 @@ class Mask
     number & @and_mask | @or_mask
   end
 
+  def apply_to_memory_address(address)
+    address_mask = ""
+    address_bit = address.to_s(2)
+    address_bit = "0" * (36 - address_bit.length) + address_bit
+    (0..35).each do |pos|
+      if @mask[pos] == '0'
+        address_mask << address_bit[pos]
+      else
+        address_mask << @mask[pos]
+      end
+    end
+
+    memory_addresses(address_mask.dup)
+  end
+
   def to_s
     "#{@mask} : #{@and_mask.to_s(2)} : #{@or_mask.to_s(2)}"
+  end
+
+  def memory_addresses(address_mask)
+    if address_mask.count("X") == 0
+      [address_mask]
+    else
+      pos = address_mask.index("X")
+      addresses = []
+      address_mask[pos] = "0"
+      addresses += memory_addresses(address_mask.dup)
+      address_mask[pos] = "1"
+      addresses += memory_addresses(address_mask.dup)
+      addresses
+    end
   end
 end
 
@@ -50,3 +79,28 @@ memory.each do |value|
 end
 
 puts "Day 14.1: sum in memory is #{sum.to_s.green}"
+
+# Part 1
+memory = {}
+mask = Mask.new("X" * 36)
+lines.each do |l|
+  parts = l.split(" = ")
+  if parts[0] == "mask"
+    mask = Mask.new(parts[1].strip)
+  else
+    index = parts[0].gsub("mem[", "").gsub("]", "").to_i
+    value = parts[1].to_i
+    memory_addresses = mask.apply_to_memory_address(index)
+
+    memory_addresses.each do |address|
+      memory[address.to_i(2)] = value
+    end
+  end
+end
+
+sum = 0
+memory.values.each do |value|
+  sum += value
+end
+
+puts "Day 14.2: sum in memory is #{sum.to_s.green}"
