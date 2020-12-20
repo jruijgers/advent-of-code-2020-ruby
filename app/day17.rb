66d953c1d99eb67fdc51803bbe4e520465b2caf6
Jeroen Ruijgers <jeroen@salandur.nl>
2020-12-20 15:25:44 -0700

    Day 17.2: conway hyper power grid

--- app/day17.rb
+++ app/day17.rb
@@ -1,12 +1,17 @@
 require "colorize"
 require "./day17/conway_cubes_power_cube"
+require "./day17/conway_hyper_power_cube"
 
 lines = []
 File.open("../input/day17.txt").each { |l| lines << l.strip }
 
 power_grid = Day17::ConwayCubesPowerGrid.parse(lines)
-
 power_grid.power_up
 
-puts "Day 17.1: after power up cycle there are #{power_grid.active_cells.to_s.green} active cells"
+puts "Day 17.1: Cubed power grid #{power_grid.active_cells.to_s.green} active cells"
+
+power_grid = Day17::ConwayHyperPowerGrid.parse(lines)
+power_grid.power_up
+
+puts "Day 17.1: Hyper power grid #{power_grid.active_cells.to_s.green} active cells"
 
--- app/day17/conway_hyper_power_cube.rb
+++ app/day17/conway_hyper_power_cube.rb
@@ -1,5 +1,5 @@
 module Day17
-  class ConwayCubesPowerGrid
+  class ConwayHyperPowerGrid
     def self.parse(lines)
       grid = {}
 
@@ -8,7 +8,7 @@ module Day17
       lines.each do |line|
         x = 0
         line.split("").each do |c|
-          grid[[x, y, 0]] = true if c == "#"
+          grid[[x, y, 0, 0]] = true if c == "#"
           x += 1
         end
         y += 1
@@ -25,22 +25,26 @@ module Day17
       @max_y = y
       @min_z = 0
       @max_z = 0
+      @min_w = 0
+      @max_w = 0
     end
 
     def power_up
       (1..6).each do |_cycle|
         new_grid = {}
 
-        ((@min_z - 1)..(@max_z + 1)).each do |z|
+        ((@min_x - 1)..(@max_x + 1)).each do |x|
           ((@min_y - 1)..(@max_y + 1)).each do |y|
-            ((@min_x - 1)..(@max_x + 1)).each do |x|
-              cell_status = @grid[[x, y, z]]
-              active_cells = active_cells_around(x, y, z)
+            ((@min_z - 1)..(@max_z + 1)).each do |z|
+              ((@min_w - 1)..(@max_w + 1)).each do |w|
+                cell_status = @grid[[x, y, z, w]]
+                active_cells = active_cells_around(x, y, z, w)
 
-              if cell_status && (active_cells == 2 || active_cells == 3)
-                new_grid[[x, y, z]] = true
-              elsif !cell_status && active_cells == 3
-                new_grid[[x, y, z]] = true
+                if cell_status && (active_cells == 2 || active_cells == 3)
+                  new_grid[[x, y, z, w]] = true
+                elsif !cell_status && active_cells == 3
+                  new_grid[[x, y, z, w]] = true
+                end
               end
             end
           end
@@ -53,16 +57,20 @@ module Day17
         @max_y += 1
         @min_z -= 1
         @max_z += 1
+        @min_w -= 1
+        @max_w += 1
       end
     end
 
     def active_cells
       active_cells = 0
 
-      (@min_x..@max_x).each do |x|
+      (@min_z..@max_z).each do |z|
         (@min_y..@max_y).each do |y|
-          (@min_z..@max_z).each do |z|
-            active_cells += 1 if @grid[[x, y, z]]
+          (@min_x..@max_x).each do |x|
+            (@min_w..@max_w).each do |w|
+              active_cells += 1 if @grid[[x, y, z, w]]
+            end
           end
         end
       end
@@ -71,15 +79,17 @@ module Day17
 
     private
 
-    def active_cells_around(x, y, z)
+    def active_cells_around(x, y, z, w)
       active_cells = 0
 
       ((x - 1)..(x + 1)).each do |x2|
         ((y - 1)..(y + 1)).each do |y2|
           ((z - 1)..(z + 1)).each do |z2|
-            next if x == x2 && y == y2 && z == z2
+            ((w - 1)..(w + 1)).each do |w2|
+              next if x == x2 && y == y2 && z == z2 && w == w2
 
-            active_cells += 1 if @grid[[x2, y2, z2]]
+              active_cells += 1 if @grid[[x2, y2, z2, w2]]
+            end
           end
         end
       end
