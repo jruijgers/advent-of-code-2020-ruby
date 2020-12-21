35d26f2dea3f348ac3160c799a1b00a7da74d60d
Jeroen Ruijgers <jeroen@salandur.nl>
2020-12-21 16:43:12 -0700

    Day 20.2: find sea monsters

--- app/day20.rb
+++ app/day20.rb
@@ -1,7 +1,7 @@
 require "colorize"
 
 class Tile
-  attr_reader :id
+  attr_reader :id, :data, :neighbours
 
   def initialize(id)
     @id = id
@@ -17,12 +17,33 @@ class Tile
     @neighbours.length
   end
 
+  def top_left_tile?
+    return false unless @neighbours.size == 2
+
+    (bottom_edge == @neighbours[0].top_edge && right_edge == @neighbours[1].left_edge) ||
+      (bottom_edge == @neighbours[1].top_edge && right_edge == @neighbours[0].left_edge)
+  end
+
+  def left_neighbour
+    @neighbours.each do |n|
+      return n if right_edge == n.left_edge
+    end
+    nil
+  end
+
+  def bottom_neighbour
+    @neighbours.each do |n|
+      return n if bottom_edge == n.top_edge
+    end
+    nil
+  end
+
   def find_neighbours(tiles)
     tiles.each do |tile|
       next if tile == self
 
-      if tile.share_edge?(@data.first.join) ||
-        tile.share_edge?(@data.last.join) ||
+      if tile.share_edge?(top_edge) ||
+        tile.share_edge?(bottom_edge) ||
         tile.share_edge?(left_edge) ||
         tile.share_edge?(right_edge)
         @neighbours << tile
@@ -31,10 +52,10 @@ class Tile
   end
 
   def share_edge?(other_edge)
-    return true if @data.first.join == other_edge
-    return true if @data.first.join == other_edge.reverse
-    return true if @data.last.join == other_edge
-    return true if @data.last.join == other_edge.reverse
+    return true if top_edge == other_edge
+    return true if top_edge == other_edge.reverse
+    return true if bottom_edge == other_edge
+    return true if bottom_edge == other_edge.reverse
     return true if left_edge == other_edge
     return true if left_edge == other_edge.reverse
     return true if right_edge == other_edge
@@ -43,18 +64,120 @@ class Tile
     false
   end
 
-  private
+  def orient_neighbours
+    @neighbours.each do |tile|
+      if bottom_edge == tile.top_edge || top_edge == tile.bottom_edge || left_edge == tile.right_edge || right_edge == tile.left_edge
+        # do nothing
+      elsif bottom_edge == tile.top_edge.reverse || top_edge == tile.bottom_edge.reverse || left_edge == tile.left_edge || right_edge == tile.right_edge
+        tile.mirror_vertical
+      elsif bottom_edge == tile.bottom_edge || top_edge == tile.top_edge || left_edge == tile.right_edge.reverse || right_edge == tile.left_edge.reverse
+        tile.mirror_horizontal
+      elsif bottom_edge == tile.bottom_edge.reverse || top_edge == tile.top_edge.reverse || left_edge == tile.left_edge.reverse || right_edge == tile.right_edge.reverse
+        tile.mirror_horizontal
+        tile.mirror_vertical
+      elsif bottom_edge == tile.right_edge || top_edge == tile.left_edge || left_edge == tile.bottom_edge.reverse || right_edge == tile.top_edge.reverse
+        tile.turn_left
+      elsif bottom_edge == tile.right_edge.reverse || top_edge == tile.left_edge.reverse || left_edge == tile.top_edge.reverse || right_edge == tile.bottom_edge.reverse
+        tile.turn_left
+        tile.mirror_vertical
+      elsif bottom_edge == tile.left_edge || top_edge == tile.right_edge || left_edge == tile.bottom_edge || right_edge == tile.top_edge
+        tile.turn_right
+        tile.mirror_vertical
+      elsif bottom_edge == tile.left_edge.reverse || top_edge == tile.right_edge.reverse || left_edge == tile.top_edge || right_edge == tile.bottom_edge
+        tile.turn_right
+      end
+    end
+  end
+
+  def mirror_vertical
+    new_data = []
+    @data.each { |d| new_data << d.reverse }
+    @data = new_data
+
+    @top_edge = nil
+    @bottom_edge = nil
+    @left_edge = nil
+    @right_edge = nil
+  end
+
+  def mirror_horizontal
+    new_data = []
+    @data.reverse.each { |d| new_data << d }
+    @data = new_data
+
+    @top_edge = nil
+    @bottom_edge = nil
+    @left_edge = nil
+    @right_edge = nil
+  end
+
+  def turn_left
+    new_data = []
+    (0..@data.length - 1).each do |i|
+      new_data[i] = []
+      @data.each do |r|
+        new_data[i] << r[@data.length - i - 1]
+      end
+    end
+    @data = new_data
+
+    @top_edge = nil
+    @bottom_edge = nil
+    @left_edge = nil
+    @right_edge = nil
+  end
+
+  def turn_right
+    new_data = []
+    (0..@data.length - 1).each do |i|
+      new_data[i] = []
+      @data.reverse.each do |r|
+        new_data[i] << r[i]
+      end
+    end
+    @data = new_data
+
+    @top_edge = nil
+    @bottom_edge = nil
+    @left_edge = nil
+    @right_edge = nil
+  end
+
+  def top_edge
+    @top_edge ||= @data.first.join
+  end
+
+  def bottom_edge
+    @bottom_edge ||= @data.last.join
+  end
 
   def left_edge
-    edge = ""
-    @data.each { |d| edge += d.first }
-    edge
+    @left_edge ||= begin
+                     edge = ""
+                     @data.each { |d| edge += d.first }
+                     edge
+                   end
   end
 
   def right_edge
-    edge = ""
-    @data.each { |d| edge += d.last }
-    edge
+    @right_edge ||= begin
+                      edge = ""
+                      @data.each { |d| edge += d.last }
+                      edge
+                    end
+  end
+
+  def ==(other)
+    other.class == self.class &&
+      other.id == id
+  end
+
+  def to_s
+    id.to_s
+  end
+
+  def data_to_s
+    @data.map { |d| d.join }.join("\n")
   end
 end
 
@@ -76,5 +199,75 @@ tiles.each do |tile|
 end
 
 product = 1
-tiles.select { |t| t.number_of_neighbours == 2 }.each { |t| product *= t.id }
+corner_tiles = tiles.select { |t| t.number_of_neighbours == 2 }
+corner_tiles.each { |t| product *= t.id }
 puts "Day 20.1: product of corner IDs is #{product.to_s.green}"
+
+tiles_to_orient = [corner_tiles.first]
+oriented_tiles = []
+while tiles_to_orient.length > 0
+  tile = tiles_to_orient.shift
+  tile.orient_neighbours
+  oriented_tiles << tile
+
+  tile.neighbours.each do |n|
+    tiles_to_orient << n unless oriented_tiles.include?(n) || tiles_to_orient.include?(n)
+  end
+end
+
+image = []
+top_left_tile = tiles.select { |t| t.top_left_tile? }.first
+bottom = top_left_tile
+begin
+  image_line = image.length
+  (0..7).each { |l| image[image_line + l] = [] }
+  left = bottom
+  begin
+    data = left.data
+    (0..7).each do |l|
+      image[image_line + l] << data[l + 1][1, 8]
+    end
+    left = left.left_neighbour
+  end while left
+  bottom = bottom.bottom_neighbour
+end while bottom
+
+image_tile = Tile.new(0)
+image.each { |i| image_tile.add_data_line(i.join) }
+
+# find see monster:
+# "                  # "
+# "#    ##    ##    ###"
+# " #  #  #  #  #  #   "
+sea_monster = [[0, 18], [1, 0], [1, 5], [1, 6], [1, 11], [1, 12], [1, 17], [1, 18], [1, 19], [2, 1], [2, 4], [2, 7], [2, 10], [2, 13], [2, 16]]
+
+contains_sea_monsters = false
+attempts = 0
+begin
+  x = 0
+  while x < image_tile.data.length - 3
+    y = 0
+    while y < image_tile.data[0].length - 20
+      contains_sea_monster = true
+      sea_monster.each do |sm|
+        contains_sea_monster &= image_tile.data[x + sm[0]][y + sm[1]] == "#"
+      end
+
+      if contains_sea_monster
+        contains_sea_monsters = true
+
+        sea_monster.each do |sm|
+          image_tile.data[x + sm[0]][y + sm[1]] = "O"
+        end
+      end
+      y += 1
+    end
+    x += 1
+  end
+  image_tile.turn_right unless contains_sea_monsters
+  attempts += 1
+end until contains_sea_monsters || attempts >= 4
+
+roughness = 0
+image_tile.data.each { |d| roughness += d.select { |d2| d2 == "#" }.length }
+puts "Day 20.1: sea roughness is #{roughness.to_s.green}"
